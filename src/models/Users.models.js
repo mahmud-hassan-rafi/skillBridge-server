5cf86085a376260c7a9138b0e0e01552a23ff34c
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2025-12-17 15:18:51 +0600

    user model done

---
+++ src/models/Users.models.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const UserSchema = new mongoose.Schema({
+  fullname: {
+    firstname: {
+      type: String,
+      required: true,
+      minlength: [3, "Firstname must be at least 3 characters"],
+    },
+    lastname: { type: String, default: "" },
+  },
+  email: { type: String, required: true, unique: true, index: true },
+  password: { type: String, required: true, select: false },
+  role: {
+    type: String,
+    enum: ["instructor", "student"],
+    default: "student",
+    required: true,
+  },
+});
+
+UserSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this._id, role: this.role },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+  return token;
+};
+
+UserSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+UserSchema.statics.hashPassword = async function (password) {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+};
+const User = mongoose.model("User", UserSchema);
+
+export default User;
