d750e1cf2b0a7f306abc3457b06322fc448c8cb1
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2025-12-25 01:05:52 +0600

    feat: create auth middlewares

---
+++ src/middlewares/Auth.middleware.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import BlacklistModel from "../models/Blacklist.model.js";
+import User from "../models/Users.models.js";
+
+export const isAuthenticatedMiddlewares = async (req, res, next) => {
+  const token =
+    req.cookies.token || req.header("Authorization")?.replace("Bearer ", "");
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const isBlacklisted = await BlacklistModel.findOne({ token: token });
+  if (isBlacklisted) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const isUserExists = await User.findById(decoded._id);
+  if (!isUserExists) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  req.user = decoded;
+  next();
+};
