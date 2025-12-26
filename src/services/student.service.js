bfcc4bb49f75e4e83d3704062baa12710a9e1144
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2025-12-26 23:21:00 +0600

    feature: create student services

---
+++ src/services/student.service.js
@@ -0,0 +1,17 @@
+import User from "../models/Users.models.js";
+
+export const createStudent = async ({ fullname, email, password, role }) => {
+  if (!fullname.firstname || !email || !password || !role) {
+    throw new Error("All fields are required");
+  } else {
+    const hashedPassword = await User.hashPassword(password);
+
+    const user = await User.create({
+      fullname,
+      email,
+      password: hashedPassword,
+      role,
+    });
+    return user;
+  }
+};
