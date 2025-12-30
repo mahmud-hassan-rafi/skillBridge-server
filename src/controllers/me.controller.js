06be4664be1ba45c38e797b5065bf1a0d6d990c0
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2025-12-31 00:12:03 +0600

    feat: add enrollments controller for user course retrieval

---
+++ src/controllers/me.controller.js
@@ -0,0 +1,15 @@
+import jwt, { decode } from "jsonwebtoken";
+import User from "../models/Users.models.js";
+
+export const enrollementsController = async (req, res) => {
+  const { token } = req.cookies;
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  const user = await User.findById(decoded._id);
+  if (!user) {
+    return res
+      .status(404)
+      .json({ message: "Login to see enrollments", navigate: "/login" });
+  }
+  res.json({ message: "success", enrolledCourses: user?.enrolledCourses });
+};
