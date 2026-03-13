180fe5c3d1904468083d573dbad1c03d47a498fe
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-03-14 03:49:09 +0600

    feat: update payment routes to remove createCheckoutSession and add webhook handling

--- src/routes/payment.route.js
+++ src/routes/payment.route.js
@@ -1,12 +1,10 @@
 import express from "express";
-import {
-  createCheckoutSession,
-  createPaymentIntent,
-} from "../controllers/payment.controller.js";
+import { createPaymentIntent } from "../controllers/payment.controller.js";
+import { stripeWebhook } from "../controllers/webhook.controller.js";
 
 const router = express.Router();
 
 router.post("/create-payment-intent", createPaymentIntent);
-router.post("/create-checkout-session", createCheckoutSession);
+router.post("/webhook", stripeWebhook);
 
 export default router;
