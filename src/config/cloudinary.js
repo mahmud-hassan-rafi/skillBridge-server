244f0fcd8fd7ea11e8820fa1a21dc154b967c513
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-04-09 13:36:17 +0600

    refactor: simplify Cloudinary configuration by removing connection logic

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,21 +1,13 @@
 import { v2 as cloudinary } from "cloudinary";
 
-let isConnected = false;
+try {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+} catch (error) {
+  console.log(error);
+}
 
-const connectCloudinary = async () => {
-  if (isConnected) return;
-
-  try {
-    cloudinary.config({
-      cloud_name: process.env.CLOUDINARY_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
-    });
-
-    isConnected = true;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export default connectCloudinary;
+export default cloudinary;
