6c56ea876f8056c0a557b0c80ea222a527d8ffb6
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-04-09 13:37:21 +0600

    refactor: comment out socket logic.

--- src/server.js
+++ src/server.js
@@ -3,8 +3,21 @@
 
 import app from "./app.js";
 import http from "http";
+import { Server } from "socket.io";
 
 const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: { origin: "https://localhost:5173" },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log("user connected");
+
+//   socket.on("disconnect", () => {
+//     console.log("User", socket.id, "is disconnected");
+//   });
+// });
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
