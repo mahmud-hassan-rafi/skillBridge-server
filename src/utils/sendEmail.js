9d211fe5bae8f57b1e5417df9955e14c2fc8617b
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-04-27 21:26:34 +0600

    feat: update sendEmail function to accept dynamic parameters for recipient, subject, and HTML content

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,11 +1,11 @@
 import { transporter } from "../config/mailTransporter.js";
 
-export const sendEmail = async () => {
+export const sendEmail = async (to, subject, html) => {
   const transporterRes = await transporter.sendMail({
     from: process.env.EMAIL_USER,
-    to: "amaricancitizen8@gmail.com",
-    subject: "Course Enrollment Success",
-    html: "<h1>You are enrolled successfully!</h1>",
+    to: to || "amaricancitizen8@gmail.com",
+    subject: subject || "Course Enrollment Success",
+    html: html || "<h1>You are enrolled successfully!</h1>",
   });
 
   console.log(transporterRes);
