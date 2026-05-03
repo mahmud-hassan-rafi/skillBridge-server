cffe1a2297e0bd8975f22486d4af2289badd7c6e
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-05-03 23:26:28 +0600

    feat: add isEnrolledTheCourseController and update enrollment routes

--- src/routes/enrollment.route.js
+++ src/routes/enrollment.route.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { getEnrollemntsController } from "../controllers/enrollment.controller.js";
+import {
+  getEnrollemntsController,
+  isEnrolledTheCourseController,
+} from "../controllers/enrollment.controller.js";
 import { isAuthenticatedMiddlewares } from "../middlewares/Auth.middleware.js";
 
 const router = express.Router();
@@ -8,6 +11,12 @@ router.get(
   "/my-enrollments",
   isAuthenticatedMiddlewares,
   getEnrollemntsController,
+);
+
+router.get(
+  "/is-enrolled/:courseId",
+  isAuthenticatedMiddlewares,
+  isEnrolledTheCourseController,
 );
 
 export default router;
